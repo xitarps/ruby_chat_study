f9af3f5871e6b60d3a040c0c001d94dbbcf903c4
xita Rps <xitarps@gmail.com>
2021-06-22 19:49:21 -0300

    Implement chat logic/js

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("ChatChannel", {
+const chatChannel = consumer.subscriptions.create("ChatChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
     alert("Conectado! chat room!");
@@ -12,9 +12,23 @@ consumer.subscriptions.create("ChatChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    let p = document.createElement('p')
+    p.innerHTML = `${data.message}`
+    document.querySelector('#messages').appendChild(p);
   },
 
-  speak: function() {
-    return this.perform('speak');
+  speak: function(message) {
+    return this.perform('speak', { message });
   }
+});
+
+document.addEventListener('turbolinks:load',()=>{
+  document.querySelector('#message_form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let message = document.querySelector('#message_to_sent').value;
+    if(message.length > 0){
+      chatChannel.speak(message);
+      document.querySelector('#message_to_sent').value = '';
+    }
+  });
 });
